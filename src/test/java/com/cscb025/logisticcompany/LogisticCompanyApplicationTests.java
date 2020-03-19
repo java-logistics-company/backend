82709c75f2d9eaa4f26a83281ad05139b0a52ac3
Ivelina Hristova <ivelinahristova5@gmail.com>
2020-03-19 22:44:43 +0200

    Innitial commit

---
+++ src/test/java/com/cscb025/logisticcompany/LogisticCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cscb025.logisticcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
